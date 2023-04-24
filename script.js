81703c2a7ac8d09532bdb4399fa5d9185834d6da
Nicole Payne <nicole1rock@gmail.com>
2023-04-24 16:57:42 -0500

    Edits to site

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let navLinks = document.getElementById ("navLinks");
 
+        // To show the menu bar when clicked on the bars symbol
         function showMenu() {
             navLinks.style.right = "0";
         }
 
+        // To hide the menu bar when clicking the x symbol
         function hideMenu() {
             navLinks.style.right = "-200px";
         }
